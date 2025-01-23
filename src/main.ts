8a3003f86c396f881ac31deb03e9e02c9ec5465d
mlordSwe <alexander.csore@gmail.com>
2025-01-23 01:11:55 +0100

    cleaned up last bit of dust: removed one line of useless code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { ApiResponse } from "./types";
-
 import { renderPage } from "./render";
 export async function getData(userInput: string): Promise<ApiResponse[]> {
   const response = await fetch(
     `https://polisen.se/api/events?locationname=${userInput}`
   );
-
   if (!response.ok) {
     throw new Error("Detta fungerar inte");
   }
